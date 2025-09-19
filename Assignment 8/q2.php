51f7616a8b5b6441be2b9a933b713402217d96df
KLP13 <kakarlaprasad2004@gmail.com>
2025-09-19 15:13:52 +0530

    Add files via upload

---
+++ Assignment 8/q2.php
@@ -0,0 +1,13 @@
+<?php
+$color = array('white', 'green', 'red');
+foreach ($color as $c) {
+    echo $c . ", ";
+}
+
+echo "<br>";
+echo "<ul>";
+foreach ($color as $c) {
+    echo "<li>$c <br> </li>";
+}
+echo "</ul>";
+?>
